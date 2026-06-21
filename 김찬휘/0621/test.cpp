30dfee07474b46814b150d1a5308fa73b2e24905
chan hwi <chanwhui08@gmail.com>
2026-06-21 21:31:48 +0900

    commit 0621

---
+++ 김찬휘/0621/test.cpp
@@ -0,0 +1,79 @@
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+//class Champion {
+//public:
+//	virtual void Qskill() {
+//		cout << "Attack!\n";
+//	}
+//};
+//class Ezreal : public Champion {
+//public:
+//	void Qskill() override {
+//		cout << "Mystic Arrow!\n";
+//	}
+//};
+//class Garen : public Champion {
+//public:
+//	void Qskill() override {
+//		cout << "Decisive Strike!\n";
+//	}
+//};
+//class Lux : public Champion {
+//public:
+//	void Qskill() override {
+//		cout << "Light Binding!\n";
+//	}
+//};
+//class Lulu : public Champion {
+//public:
+//	void Qskill() override {
+//		cout << "Glitterlance!\n";
+//	}
+//};
+//class Ash : public Champion {
+//public:
+//	void Qskill() override {
+//		cout << "Ranger's Focus!\n";
+//	}
+//};
+//int main(void) {
+//	Champion _champion;
+//	Ezreal _ezreal;
+//	Garen _garen;
+//	Lux _lux;
+//	Lulu _lulu;
+//	Ash _ash;
+//
+//	_champion.Qskill();
+//	_ezreal.Qskill();
+//	_garen.Qskill();
+//	_lux.Qskill();
+//	_lulu.Qskill();
+//	_ash.Qskill();
+//}
+
+class Student {
+public:
+	string name;
+	int Kor_Score;
+	int Eng_Score;
+	int Math_Score;
+};
+
+class Score {
+	int getTotal(int Kor_Score, int Eng_Score, int Math_Score) {
+		int result;
+		result = Kor_Score + Eng_Score + Math_Score;
+	}
+	double getAverage(int total) {
+
+	}
+
+};
+
+int main(void) {
+	
+}
